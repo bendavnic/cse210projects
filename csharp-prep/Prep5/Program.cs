7bdad2b533cefa36d76f5669c34bfe42a2dafa1b
Benjamin Nicholas <bendavnic@byui.edu>
2024-09-24 20:48:50 -0600

    prep assignment 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcomeMessage();
+        string name = PromptUserName();
+        int square = SquareNumber(PromptUserNumber());
+        DisplayResult(name, square);
+    }
+
+    static void DisplayWelcomeMessage()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        return Console.ReadLine();
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter a number: ");
+        return int.Parse(Console.ReadLine());
+    }
+
+    static int SquareNumber(int x)
+    {
+        return x * x;
+    }
+    
+    static void DisplayResult(string name, int number)
+    {
+        Console.WriteLine($"{name}, the square of your number is {number}.");
     }
 }
