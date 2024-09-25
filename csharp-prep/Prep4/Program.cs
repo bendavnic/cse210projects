5402a003031c01811b7d1c239c2d97511a547cb2
Benjamin Nicholas <bendavnic@byui.edu>
2024-09-24 18:47:20 -0600

    prep assignment 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        //get list of numbers
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        int input = -1;
+        while (input != 0)
+        {
+            Console.Write("Enter number: ");
+            input = int.Parse(Console.ReadLine());
+            if (input != 0)
+            {
+                numbers.Add(input);
+            }
+        }
+
+        //get sum of list
+        int sum = 0;
+        foreach (int i in numbers)
+        {
+            sum += i;
+        }
+        Console.WriteLine($"The sum is: {sum}");
+        
+        //get average of list
+        int average = sum / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        //get largest number in list
+        int max = numbers.Max();
+        Console.WriteLine($"The largest number is: {max}");
     }
 }
